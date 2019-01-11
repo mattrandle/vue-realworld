201f7e9edf4a8d199c9af734f112e9fde5441440
Matt Randle <matt_randle@yahoo.com>
2019-01-11 16:29:49 +0000

    Register link

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,7 @@
             <div class="col-md-6 offset-md-3 col-xs-12">
                 <h1 class="text-xs-center">Sign in</h1>
                 <p class="text-xs-center">
-                <router-link to="/signup">Need an account ?</router-link>
+                <router-link to="/register">Need an account ?</router-link>
                 </p>
 
                 <ul class="error-messages" v-if="error">
